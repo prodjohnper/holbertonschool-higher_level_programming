ee3af42ec216964c3675beb44c468e63224a8983
prodjohnper <jonathanperez9743@gmail.com>
2024-01-18 23:47:46 -0400

    Add - Created function that prints alphabet backwards in lower and uppercase

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-print(''.join(chr(i - (i % 2) * 32) for i in range(122, 96, -1)))
+c = 0
+for char in range(ord('z'), ord('a') - 1, -1):
+    print('{}'.format(chr(char - c)), end='')
+    c = 32 if c == 0 else 0
