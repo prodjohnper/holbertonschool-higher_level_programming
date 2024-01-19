568d79b347d3a6891d971987125d3f3d4cb24408
prodjohnper <jonathanperez9743@gmail.com>
2024-01-18 22:16:18 -0400

    Add - Created uppercase function

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+def uppercase(str):
+
+    for char in str:
+        if 97 <= ord(char) <= 122:
+            uppercase_char = chr(ord(char) - 32)
+        else:
+            uppercase_char = char
+        print(uppercase_char, end='')
+
+    print()
