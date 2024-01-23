1af8a2c6e0f1048e812f96740c6bdbc62086406d
prodjohnper <jonathanperez9743@gmail.com>
2024-01-22 22:39:49 -0400

    Add - Created calculator script

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,30 +2,26 @@
 import sys
 from calculator_1 import add, sub, mul, div
 
+argc = len(sys.argv)
 
-def calculate_result(a, operator, b):
-    if operator == '+':
-        result = add(a, b)
-    elif operator == '-':
-        result = sub(a, b)
-    elif operator == '*':
-        result = mul(a, b)
-    elif operator == '/':
-        result = div(a, b)
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+if argc != 4:
+    print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+    exit(1)
 
-    print("{} {} {} = {}".format(a, operator, b, result))
+operator = str(sys.argv[2])
+a = int(sys.argv[1])
+b = int(sys.argv[3])
 
+if operator == '+':
+    result = add(a, b)
+elif operator == '-':
+    result = sub(a, b)
+elif operator == '*':
+    result = mul(a, b)
+elif operator == '/':
+    result = div(a, b)
+else:
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
 
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
-
-    calculate_result(a, operator, b)
+print('{} {} {} = {}'.format(a, operator, b, result))
