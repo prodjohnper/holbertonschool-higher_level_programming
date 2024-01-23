1167543fb45b784a2d9b03a352818b102ca6accc
prodjohnper <jonathanperez9743@gmail.com>
2024-01-23 12:53:26 -0400

    Add - Create print matrix function

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,9 +3,9 @@
 def print_matrix_integer(matrix=[[]]):
 
     for row in matrix:
-        for nums in range(len(row)):
-            if nums == len(row) - 1:
-                print('{:d}'.format(nums), end='')
+        for i, num in enumerate(row):
+            if i == len(row) - 1:
+                print('{:d}'.format(num), end='')
             else:
-                print('{:d}'.format(nums), end='')
+                print('{:d}'.format(num), end='')
         print()
