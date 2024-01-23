59d45e4d8c19e3cbd8cf3db5deca97a04ac4f999
prodjohnper <jonathanperez9743@gmail.com>
2024-01-22 22:52:27 -0400

    Add - Created calculator script

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
-import sys
 from calculator_1 import add, sub, mul, div
+import sys
 
-argc = len(sys.argv)
 
-if argc != 4:
-    print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-    exit(1)
-
-operator = str(sys.argv[2])
-a = int(sys.argv[1])
-b = int(sys.argv[3])
-
-if operator == '+':
-    result = add(a, b)
-elif operator == '-':
-    result = sub(a, b)
-elif operator == '*':
-    result = mul(a, b)
-elif operator == '/':
-    result = div(a, b)
-else:
+def usage_msg():
     print("Unknown operator. Available operators: +, -, * and /")
     exit(1)
 
-print('{} {} {} = {}'.format(a, operator, b, result))
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
+        exit(1)
+
+    a = int(sys.argv[1])
+    operator = str(sys.argv[2])
+    b = int(sys.argv[3])
+
+    if operator == "+":
+        result = add(a, b)
+    elif operator == "-":
+        result = sub(a, b)
+    elif operator == "*":
+        result = mul(a, b)
+    elif operator == "/":
+        result = div(a, b)
+    else:
+        usage_msg()
+
+    print("{} {} {} = {}".format(a, operator, b, result))
