b7131cab97b39cb7a37e2a058fdf8987d2a07710
prodjohnper <jonathanperez9743@gmail.com>
2024-01-22 21:32:56 -0400

    Add - Created hidden_discovery script

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 
     list_str = dir(hidden_4)
 
-    for _ in range(0, len(str_list)):
+    for _ in range(0, len(list_str)):
         string = list_str[_]
         if string[:2] != "__" != string[-2:]:
             print(string)
