d398450ce54ec0289fce89812afa10f741d9120b
prodjohnper <jonathanperez9743@gmail.com>
2024-01-30 12:32:58 -0400

    Add - Created safe print list integers function

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         count = 0
         for i in range(x):
-            if type(my_list[i] is int):
+            if type(my_list[i]) is int:
                 print('{:d}'.format(my_list[i]), end='')
                 count += 1
             else:
