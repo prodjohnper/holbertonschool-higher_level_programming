17b40b7130416d39fc860747e1e1e0eb15064723
prodjohnper <JonathanPerez9743@gmail.com>
2024-01-31 09:43:39 -0400

    Fix - Fixed codestyle error

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-'''
-	Write an empty class Square that defines a square.
+''' Write an empty class Square that defines a square.
 '''
 
 
 class Square:
-    '''
-            This is an empty class Square.
-    '''
+    ''' This is an empty class Square.
+'''
     pass
