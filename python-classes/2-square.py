9e459db777ad76dc5dd3efefdb13f001c9dca365
prodjohnper <JonathanPerez9743@gmail.com>
2024-01-31 10:19:43 -0400

    Add - Created class Square with conditionals

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' Write a class Square that defines a square.
+'''
+
+
+class Square:
+    ''' Class Square definition.
+'''
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
