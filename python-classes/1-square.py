5ae825992849b55747352800bb6dced072eefddf
prodjohnper <JonathanPerez9743@gmail.com>
2024-01-31 10:05:55 -0400

    Add - Created class Square with private instance size

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''Write a class Square that defines a square.
+'''
+
+
+class Square:
+    ''' Class Square definition.
+'''
+
+    def __init__(self, size):
+        self.__size = size
