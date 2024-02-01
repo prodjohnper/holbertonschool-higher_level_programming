1cf3ed66f5e31fc2617fda4bfb201b5dac9d2085
prodjohnper <JonathanPerez9743@gmail.com>
2024-01-31 21:36:07 -0400

    Fix - Fixed error in docstring

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,11 +1,34 @@
 #!/usr/bin/python3
-''' Function that adds two integers.
-    a and b must be integers or floats, otherwise raise a TypeError exception.
-    If a or b are floats, the have to be casted before returning them.
+''' 
+    Module docstring with 5 lines.
+
+    This module provides a function for adding two integers.
+
+    The `add_integer` function takes two arguments, `a` and `b`,
+    which must be integers or floats. If a or b are floats,
+    they will be casted before returning the sum.
+
+    Usage examples:
+
+        >>> add_integer(2, 3)
+        5
+
+        >>> add_integer(100, -2)
+        98
 '''
 
 
 def add_integer(a, b=98):
+    '''
+    Function docstring with 3 lines.
+
+    Args:
+        a (int or float): The first integer or float.
+        b (int or float, optional): The second integer or float. Defaults to 98.
+
+    Returns:
+        int: The sum of a and b.
+    '''
 
     if not isinstance(a, (int, float)):
         raise TypeError('a must be an integer')
