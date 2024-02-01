b56badc427df0edacb154c7270c61e1a293382d1
prodjohnper <JonathanPerez9743@gmail.com>
2024-01-31 21:44:58 -0400

    Fix - Fixed error in docstring

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 ''' Function to add two integers.
+
     They're then casted and returned.
+    
 '''
 
 
 def add_integer(a, b=98):
     ''' Adds two integers.
-
         Returns: The sum of a and b.
-
     '''
 
     if not isinstance(a, (int, float)):
