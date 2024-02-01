7e9ecf9ae9c7f5d2da9958eeca3ff49a8f66ca36
prodjohnper <JonathanPerez9743@gmail.com>
2024-02-01 19:48:35 -0400

    Fix - Refactored code

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -11,9 +11,13 @@ def add_integer(a, b=98):
         Returns: The sum of a and b.
     '''
 
-    if not isinstance(a, (int, float)):
-        raise TypeError('a must be an integer')
-    if not isinstance(b, (int, float)):
-        raise TypeError('b must be an integer')
+    if a is None or b is None:
+        raise TypeError(
+            "unsupported operand type(s) for +: 'NoneType' and 'int'")
+
+    if type(a) is not int and type(a) is not float:
+        raise TypeError('a must be and integer')
+    if type(b) is not int and type(b) is not float:
+        raise TypeError('b must be and integer')
 
     return int(a) + int(b)
