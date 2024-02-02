fcf451d0ebf366e0b00cba48b4b4cb585eeb89d3
prodjohnper <JonathanPerez9743@gmail.com>
2024-02-01 21:02:35 -0400

    Fix - Refactored code

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,34 +1,37 @@
 #!/usr/bin/python3
+''' Function that divides all elements of a matrix. '''
+
 
 def matrix_divided(matrix, div):
+    '''Divides all elements of a matrix.
+
+    Args:
+            matrix (list of lists): Matrix of integers or floats.
+            div (int or float): The divisor.
+
+    Returns:
+            list of lists: New matrix with elements divided by div, rounded to 2 decimal places.
+
+    Raises:
+            TypeError: If matrix is not a list of lists of integers/floats, if each row is not of the same size, or if div is not a number.
+            ZeroDivisionError: If div is equal to 0.
+    '''
+
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError(
+            'matrix must be a matrix (list of lists) of integers/floats')
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError('Each row of the matrix must have the same size')
+
+    if not isinstance(div, (int, float)):
+        raise TypeError('div must be a number')
 
     if div == 0:
-        raise ZeroDivisionError("division by zero")
+        raise ZeroDivisionError('division by zero')
 
-    if type(div) is not int and type(div) is not float:
-        raise TypeError("div must be a number")
+    # Divide each element of the matrix by div, rounded to 2 decimal places
+    result_matrix = [[round(element / div, 2)
+                      for element in row] for row in matrix]
 
-    temp = len(matrix[0])
-
-    for row in matrix:
-        if type(row) is not list:
-            raise TypeError(
-                "matrix must be a matrix (list of lists) of integers/floats")
-        if temp != len(row):
-            raise TypeError("Each row of the matrix must have the same size")
-        temp = len(row)
-
-    new_list = [row[:] for row in matrix]
-    err_msg = "matrix must be a matrix (list of lists) of integers/floats"
-
-    for row in range(len(matrix)):
-        for column in range(len(matrix[row])):
-            if (
-                type(matrix[row][column]) is not int
-                and type(matrix[row][column]) is not float
-            ):
-                raise TypeError(err_msg)
-            result = (matrix[row][column]/div)
-            new_list[row][column] = ('{:.2f}'.format(result))
-
-    return new_list
+    return result_matrix
