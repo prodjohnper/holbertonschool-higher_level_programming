fa5180e9b8e9263dc70b05ceb12a3337426a2260
prodjohnper <JonathanPerez9743@gmail.com>
2024-02-01 22:18:09 -0400

    Add - Created say my name function

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
+"""
+3-say_my_name
+
+Function that prints first name and last name.
+"""
+
 
 def say_my_name(first_name, last_name=""):
+    """	Description: prints first name and last name
+        first_name (str): The name string
+        last_name (str): The last name, default is '' """
 
-    error1 = "first_name must be a string"
-    error2 = "last_name must be a string"
+    error_a = "first_name must be a string"
+    error_b = "last_name must be a string"
 
     if first_name is None:
-        raise TypeError(error1)
+        raise TypeError(error_a)
     if last_name is None:
-        raise TypeError(error2)
-
+        raise TypeError(error_b)
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
