c8af799d8031ebc903c3bf383c41dd89c5a869b0
prodjohnper <JonathanPerez9743@gmail.com>
2024-02-07 17:59:57 -0400

    Fix

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -7,7 +7,7 @@
 
 
 class Rectangle:
-    """ Private instance attributes: width, height """
+    """ Class that defines a Rectangle """
 
     def __init__(self, width=0, height=0):
         self.width = width
@@ -20,9 +20,9 @@ class Rectangle:
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
-            raise TypeError('width must be an integer')
+            raise TypeError("width must be an integer")
         elif value < 0:
-            raise ValueError('width must be >=0')
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -32,7 +32,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
-            raise TypeError('height must be an integer')
+            raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError('height must be >= 0')
+            raise ValueError("height must be >= 0")
         self.__height = value
