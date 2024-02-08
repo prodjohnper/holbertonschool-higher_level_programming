9d2bf66e2fc25044233dec2569c7204f8086685f
prodjohnper <JonathanPerez9743@gmail.com>
2024-02-07 21:03:34 -0400

    Add - Created class MyList and doctest

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+    1-my_list.py
+    
+    Description: Create a class MyList that inherits from list.
+'''
+
+
+class MyList(list):
+    '''
+        Class MyList that inherits from list
+    '''
+
+    def print_sorted(self):
+        sorted_list = sorted(self)
+        print(sorted_list)
