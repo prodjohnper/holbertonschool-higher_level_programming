4d1eaf0b4062f634432684b72263101b7f3ef7fc
prodjohnper <JonathanPerez9743@gmail.com>
2024-02-07 21:43:20 -0400

    Add - Created class BaseGeometry that raises an exception with a msg

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+    6-base_geometry.py
+
+    Description: Write a class BaseGeometry.
+'''
+
+
+class BaseGeometry:
+    '''
+        Public instance method: def area(self): that raises
+        an Exception with the message area() is not implemented. '''
+
+    def area(self):
+        raise Exception('area() is not implemented')
