71f10a68c1dc580a6bf204b746dd7115bc287bc6
prodjohnper <JonathanPerez9743@gmail.com>
2024-02-08 19:43:44 -0400

    Add - Created function that reads a text file and prints it to stdout

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+'''
+    0-read_file.py
+    Description: Write a function that reads a
+    text file (UTF8) and prints it to stdout
+'''
+
+
+def read_file(filename=""):
+    '''
+        Function that reads a text file (UTF8) and prints it to stdout
+    '''
+    with open(filename, encoding="UTF-8") as f:
+        read_data = f.read()
+
+    print(read_data, end='')
