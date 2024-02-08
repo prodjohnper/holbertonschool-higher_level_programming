80d6173b1769d834cee6fac316ed1f2ec477152a
prodjohnper <JonathanPerez9743@gmail.com>
2024-02-07 21:39:29 -0400

    Add - Created empty class BaseGeometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+    5-base_geometry.py
+
+    Description: Write an empty class BaseGeometry.
+'''
+
+
+class BaseGeometry:
+    '''
+        Empty class BaseGeometry
+    '''
+
+    pass
