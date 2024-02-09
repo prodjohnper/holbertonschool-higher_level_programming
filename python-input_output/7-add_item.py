e0000b3004939d38ff5e65f88bf4f20566d6f504
prodjohnper <JonathanPerez9743@gmail.com>
2024-02-08 23:18:00 -0400

    Add - Created script that adds all arguments     to a Python list, and then save them to a file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,18 +4,32 @@
     Description: Write a script that adds all arguments
     to a Python list, and then save them to a file
 '''
+import json
 import sys
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = 'add_items.json'
+
+def save_to_json_file(my_obj, filename):
+    ''' Write a function that writes an Object to a
+        text file, using a JSON representation
+    '''
+    with open(filename, 'w') as f:
+        json.dump(my_obj, f)
+
+
+def load_from_json_file(filename):
+    '''
+        Function that creates an Object from a “JSON file"
+    '''
+    with open(filename, 'r') as f:
+        return json.load(f)
+
 
 try:
-    items = load_from_json_file(filename)
-except FileNotFoundError:
-    items = []
+    obj = load_from_json_file("add_item.json")
+except:
+    obj = []
 
-for arg in sys.argv[1:]:
-    items.append(arg)
+for _ in sys.argv[1:]:
+    obj.append(_)
 
-save_to_json_file(items, filename)
+save_to_json_file(obj, "add_item.json")
