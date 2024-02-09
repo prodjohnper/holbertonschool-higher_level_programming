049e1b08b611a006431c21b10e3e95b81030a290
prodjohnper <JonathanPerez9743@gmail.com>
2024-02-08 23:40:42 -0400

    Add - Created class Student

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+'''
+    9-student.py
+    Description: Define a class Student that defines
+    a student by first_name, last_name, and age.
+'''
+
+
+class Student:
+    ''' class Student that defines a student
+        by first name, last name and age
+    '''
+
+    def __init__(self, first_name, last_name, age):
+        '''
+            Instantiation with first_name, last_name and age
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_join(self):
+        '''
+            Retrieves a dictionary representation of a Student instance
+        '''
+        return self.__dict__
