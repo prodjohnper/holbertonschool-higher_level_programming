bf5fe152594554784a8b970ba9f34e1956f1fd47
prodjohnper <JonathanPerez9743@gmail.com>
2024-02-08 21:46:18 -0400

    Add - Create a function that returns an object     (Python data structure) represented by a JSON string

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+'''
+    4-from_json_string.py
+    Description: Write a function that returns an object
+    (Python data structure) represented by a JSON string.
+'''
+import json
+
+
+def from_json_string(my_str):
+    ''' Write a function that returns an object (Python
+        data structure) represented by a JSON string.
+    '''
+    return json.loads(my_str)
