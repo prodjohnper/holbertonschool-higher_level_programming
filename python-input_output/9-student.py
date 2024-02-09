671217c49f6b734dc99bc06e9052266df588a406
prodjohnper <JonathanPerez9743@gmail.com>
2024-02-08 23:45:01 -0400

    Add - Created class Student

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -19,8 +19,12 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_join(self):
+    def to_json(self):
         '''
             Retrieves a dictionary representation of a Student instance
         '''
-        return self.__dict__
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
