f73dc799f38775731e11abe4dcb67e182e3f43a8
prodjohnper <JonathanPerez9743@gmail.com>
2024-02-08 21:40:38 -0400

    Fix - Fixed import before documentation

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-import json
-
 '''
     3-to_json_string.py
     Description: Write a function that returns the
     JSON representation of an object (string)
 '''
+import json
 
 
 def to_json_string(my_obj):
