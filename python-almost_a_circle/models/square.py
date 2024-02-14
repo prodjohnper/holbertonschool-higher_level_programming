05fe95fc940bf83b62c7ecdece9fab9c6252e33e
prodjohnper <JonathanPerez9743@gmail.com>
2024-02-14 13:52:38 -0400

    Add - Created class Square and implemented class constructor and str representation

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+    square.py
+    Description: Class Square that
+    inherits from class Rectangle
+'''
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''
+        Class Square that inherits from class Rectangle
+    '''
+
+    def __init__(self, size, x=0, y=0, id=None):
+        '''
+            Class Constructor
+        '''
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        id = self.id
+        x = self.x
+        y = self.y
+        s = self.width
+
+        return f'[Square] ({id}) {x}/{y} - {s}'
