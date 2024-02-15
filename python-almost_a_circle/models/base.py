94d759810557cdbb611e93a9be435c9e7e18d80e
prodjohnper <JonathanPerez9743@gmail.com>
2024-02-15 13:51:52 -0400

    Update - Implemented JSON string representation

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,6 +4,7 @@
     Description: This class will be the “base”
     of all other classes in this project
 '''
+import json
 
 
 class Base:
@@ -21,3 +22,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects  # Increment and assign new id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''
+            Return JSON string representation of list_dictionaries
+        '''
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+
+        return json.dumps(list_dictionaries)
