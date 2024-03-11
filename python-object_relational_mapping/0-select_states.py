e8a1e3c2c30526595587dbdfc1f10cb5ccd576d8
prodjohnper <JonathanPerez9743@gmail.com>
2024-03-11 18:19:36 -0400

    Add - Created script that lists all states from the database hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,12 +9,16 @@ from sys import argv
 
 if __name__ == "__main__":
 
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
     conn = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=argv[1],
-        password=argv[2],
-        database=argv[3]
+        user=username,
+        password=password,
+        database=database
     )
 
     cursor = conn.cursor()
