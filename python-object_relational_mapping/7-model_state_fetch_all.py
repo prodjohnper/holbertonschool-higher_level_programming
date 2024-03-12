6a586d28bee049f25e4390dcab1ef65deb1073fb
prodjohnper <JonathanPerez9743@gmail.com>
2024-03-11 22:37:25 -0400

    Add - Created script that lists all State objects from the database hbtn_0e_6_usa

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,33 @@
-# This is the content of 7-model_state_fetch_all.py.
+#!/usr/bin/python3
+'''
+    7-model_state_fetch_all.py
+    Description: Script that lists all State
+    objects from the database hbtn_0e_6_us
+'''
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Create and bind SQLAlchemy engine to session
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
+
+    # Create session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query to select all State objects
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print(f'{state.id}: {state.name}')
+
+    session.close()
