cfb2e5c12a44c2c0ce8250e23a56b4f34c95f78e
prodjohnper <JonathanPerez9743@gmail.com>
2024-03-11 23:04:29 -0400

    Addd - Created script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,36 @@
-# This is the content of 10-model_state_my_get.py.
+#!/usr/bin/python3
+'''
+    10-model_state_my_get.py
+    Description: Prints the State object with the name
+    passed as argument from the database hbtn_0e_6_usa
+'''
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Create and bind SQLAlchemy engine to session
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
+
+    # Create session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query to select the State object with state name
+    state = session.query(State).filter(State.name == state_name).first()
+
+    if state:
+        print(state.id)
+    else:
+        print('Not found')
+
+    session.close()
