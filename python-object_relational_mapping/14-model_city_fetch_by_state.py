3a5f95065f57f5a002614d921687df07552a8f66
prodjohnper <JonathanPerez9743@gmail.com>
2024-03-12 20:24:33 -0400

    Add - Created class City and script that prints all City objects from the database hbtn_0e_14_usa

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''
+    '''
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Create and bind SQLAlchemy engine to session
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}',
+        pool_pre_ping=True)
+
+    # Create all tables defined in Base
+    Base.metadata.create_all(engine)
+
+    # Create session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query to select all City objects
+    cities = session.query(City).order_by(City.id).all()
+
+    for city in cities:
+        state_name = session.query(State).filter_by(
+            id=city.state_id).first().name
+        print(f'{state_name}: ({city.id}) {city.name}')
+
+    session.close()
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,1 +1,33 @@
-# This is the content of model_city.py, 14-model_city_fetch_by_state.py.
+#!/usr/bin/python3
+'''
+    model_city.py
+
+    Description: Class City that inherits from Base
+'''
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    '''
+        City class that inherits from Base
+    '''
+    __tablename__ = 'cities'
+
+    id = Column(
+        Integer,
+        autoincrement=True,
+        nullable=False,
+        primary_key=True
+    )
+
+    name = Column(
+        String(128),
+        nullable=False
+    )
+
+    state_id = Column(
+        Integer,
+        ForeignKey('states.id'),
+        nullable=False
+    )
