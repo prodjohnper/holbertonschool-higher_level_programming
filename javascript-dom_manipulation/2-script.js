aa901b548ef8d4023ff651c5bf2ce311848679c2
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-21 09:45:29 -0400

    Add - Created script that adds the class red to the header element   when the user clicks on the tag with id red_header

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -4,3 +4,10 @@
   Script that adds the class red to the header element
   when the user clicks on the tag with id red_header.
 */
+
+// Adds the class red when the tag with id `red_header` is clicked
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('red_header').addEventListener('click', function () {
+      document.querySelector('header').classList.add('red');
+  });
+});
