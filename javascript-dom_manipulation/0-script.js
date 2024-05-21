ed4b9af39b7b96666d877fdf1f152123ef28ff50
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-21 09:21:01 -0400

    Add - Created script that updates the `header` element color to red

--- javascript-dom_manipulation/0-script.js
+++ javascript-dom_manipulation/0-script.js
@@ -3,3 +3,6 @@
 
   Script that updates the text color of the header element to red (#FF0000).
 */
+
+// Update the text color of the header element to red
+document.querySelector('header').style.color = '#FF0000';
