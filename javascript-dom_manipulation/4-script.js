696472b1176ba39a47c7256a13202425097c1291
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-21 10:11:32 -0400

    Add - Created script that adds a <li> element to a list when the user clicks on the element with id add_item

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -3,3 +3,12 @@
 
   Script that adds a <li> element to a list when the user clicks on the element with id add_item.
 */
+
+// Adds a <li> element to the list when the tag with id `add_item` is clicked
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('add_item').addEventListener('click', function () {
+    const item = document.createElement('li');
+    item.appendChild(document.createTextNode('Item'));
+    document.querySelector('ul.my_list').appendChild(item);
+  });
+});
