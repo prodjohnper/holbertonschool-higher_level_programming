6708da83edaf8cbd08ca471ea5067a4cef9621df
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-21 10:23:53 -0400

    Add - Created script that updates the text of the header element to `New Header!!!`   when the user clicks on the element with id update_header

--- javascript-dom_manipulation/5-script.js
+++ javascript-dom_manipulation/5-script.js
@@ -4,3 +4,10 @@
   Script that updates the text of the header element to `New Header!!!`
   when the user clicks on the element with id update_header.
 */
+
+// Update the text of the header element to `New Header!!!` when the tag with id `update_header` is clicked
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('update_header').addEventListener('click', function () {
+    document.querySelector('header').textContent = 'New Header!!!';
+  });
+});
