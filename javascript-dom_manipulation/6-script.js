040e1e751f404e4eff5758c539769e7731d41d80
prodjohnper <JonathanPerez9743@gmail.com>
2024-05-21 10:41:22 -0400

    Add - Created script that fetches the character name from this   URL: `https://swapi-api.hbtn.io/api/people/5/?format=json`

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -4,3 +4,18 @@
   Script that fetches the character name from this
   URL: `https://swapi-api.hbtn.io/api/people/5/?format=json`.
 */
+
+// Fetch the character name from the URL and display it in the HTML tag div with `character` id
+document.addEventListener('DOMContentLoaded', function () {
+  // Fetch the character name from the URL
+  fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+    // Parse the response as JSON
+    .then(response => response.json())
+    .then(data => {
+      // Display the character name in the HTML tag div with `character` id
+      const name = data.name;
+      document.getElementById('character').textContent = name;
+    })
+  // Log any errors to the console
+    .catch(error => console.log(error.message));
+});
